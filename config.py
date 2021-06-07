8b01127386ce46b189e2e18feed445722444e80a
pjparis42 <85468171+pjparis42@users.noreply.github.com>
2021-06-06 19:37:50 -0700

    Update and rename config.py.sample to config.py
    unsure of lines 16-18

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-mnemonic = os.environ.get('MNEMONIC', 'paste your seed here')
-target_percent = 80
-trigger_at_percent = 85
+mnemonic = os.environ.get('MNEMONIC', 'terra1sztndfmv2h28ucz2u5855av3g404x4tdvh4ysz')
+target_percent = 35
+trigger_at_percent = 40
 
 # (True or False)
 enabled_auto_borrow = True
-auto_borrow_at_percent = 70
+auto_borrow_at_percent = 30
 NETWORK = 'MAINNET'
 
 # NOTIFICATIONS (True or False)
